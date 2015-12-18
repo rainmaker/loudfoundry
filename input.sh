99169b63004e0dd7870fcf51bbbb0e776ecd45e0
Raina Masand <rmasand@pivotal.io>
2015-12-18 11:01:07 -0800

    Add loop to record 1 minute snippets
    - At random intervals < 10min
    - Make sure we exit on SIGINT because sox doesn't care

--- input.sh
+++ input.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-set -x
+trap "echo 'catching ctrl-c'; exit 0" 2
 
-# record a minute of audio
-sox -d cf.wav trim 0 60
+while true; do
+  # record a minute of audio
+  echo 'Recording a minute of audio...'
+  sox -d cf.wav trim 0 60
 
-# find the average amplitude
-sox cf.wav -n stat 2>&1 | sed -n '9p' | awk '{ print $3 }' >> loudness
+  echo $RANDOM | cut -c1-3 | sleep
+
+  # find the average amplitude
+  sox cf.wav -n stat 2>&1 | sed -n '9p' | awk '{ print $3 }' >> loudness
+done
