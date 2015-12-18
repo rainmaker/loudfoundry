2548c017a458de5fe3aed50143a7c398e08610e0
Raina Masand <rmasand@pivotal.io>
2015-12-18 10:43:15 -0800

    Write loudness to file

--- input.sh
+++ input.sh
@@ -2,4 +2,8 @@
 
 set -x
 
-sox -d cf.wav trim 0 10
+# record a minute of audio
+sox -d cf.wav trim 0 60
+
+# find the average amplitude
+sox cf.wav -n stat 2>&1 | sed -n '9p' | awk '{ print $3 }' >> loudness
